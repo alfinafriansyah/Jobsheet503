b72c5f71bb5e65ba2aabddd664b845c6aee831a0
Alfin Afriansyah <143513533+alfinafriansyah@users.noreply.github.com>
2023-09-26 11:37:57 +0700

    Add files via upload

---
+++ PemilihanPercobaan103.java
@@ -0,0 +1,16 @@
+package Jobsheet5;
+
+import java.util.Scanner;
+
+public class PemilihanPercobaan103 {
+    public static void main(String[] args){
+        Scanner input03 = new Scanner(System.in);
+        System.out.print("Masukkan angka:  ");
+        int angka = input03.nextInt();
+        if (angka % 2 == 0){
+            System.out.println("Angka " +angka+ " bilangan genap");
+        }else{
+            System.out.println("Angka " +angka+ " bilangan ganjil");
+        }
+    }
+}
